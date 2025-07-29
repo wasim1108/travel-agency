9462468c0feb2fa5e88a39078d8b0a77fb76fa80
Wasim <wasim.awa1108@gmail.com>
2025-07-29 17:26:51 +0530

    fix: logout button to signout existing user

--- app/routes/root/travel-page.tsx
+++ app/routes/root/travel-page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
+import { logoutUser } from '~/appwrite/auth'
 
 const TravelPage = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    // Perform logout logic here, e.g., clear user session, redirect to login page
+    console.log('User logged out');
+    await logoutUser(); // Call the logout function
+    navigate('/sign-in'); // Redirect to sign-in page after logout
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <button
+        onClick={handleLogout}
+        className="cursor-pointer"
+      >
+        <img src="/assets/icons/logout.svg" alt="Logout" className="size-6" />
+      </button>
+
+      <button
+        onClick={() => navigate('/dashboard')}
+        >
+        Dashboard
+      </button>
+    </div>
   )
 }
 
